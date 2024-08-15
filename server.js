91a57e6c53773def3df897c3ec837aa42992633b
ItaloFdaS <italofelipe26@hotmail.com>
2024-08-15 20:46:37 -0300

    corrigindo erro app.post

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express, { request } from "express";
-
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -7,6 +7,8 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/usuarios", async (req, res) => {
   let users = [];
@@ -26,7 +28,7 @@ app.get("/usuarios", async (req, res) => {
 });
 
 app.post("/usuarios", async (req, res) => {
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email: req.body.email,
       age: req.body.age,
@@ -34,7 +36,7 @@ app.post("/usuarios", async (req, res) => {
     },
   });
 
-  res.status(201).json({ user });
+  res.status(201).json(req.body);
 });
 
 app.put("/usuarios/:id", async (req, res) => {
